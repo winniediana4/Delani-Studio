adb9b6f2eb92c1455755a237ec124dbb1479e3d6
winniediana4 <deinawinnie84@gmail.com>
2021-04-19 22:09:44 +0300

    add style to portfolio

---
+++ js/scripts.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+  $("#design").click(function(){
+    $(".design").fadeIn();
+    $("#design").fadeOut();
+  });
+  $(".design").click(function(){
+    $("#design").fadeIn();
+    $(".design").fadeOut();
+  });
+});
+$(document).ready(function(){
+  $("#development").click(function(){
+    $(".development").fadeIn();
+    $("#development").fadeOut();
+  });
+  $(".development").click(function(){
+    $("#development").fadeIn();
+    $(".development").fadeOut();
+  });
+});
+$(document).ready(function(){
+  $("#product").click(function(){
+    $(".product").fadeIn();
+    $("#product").fadeOut();
+  });
+  $(".product").click(function(){
+    $("#product").fadeIn();
+    $(".product").fadeOut();
+  });
+});
+
+$(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlay1").show();
+  }).mouseout(function(){
+    $("#overlay1").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("form")
+})
